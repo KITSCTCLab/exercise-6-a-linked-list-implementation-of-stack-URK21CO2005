d5abb010d455ed2cbd82e1f14aea17561716e46a
URK21CO2005 <110015219+URK21CO2005@users.noreply.github.com>
2022-09-12 20:50:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,33 +4,50 @@ class Node:
     self.next = None
 
 
-class Stack:
+class Queue:
   def __init__(self):
     self.head = None
+    self.last = None
 
-  def push(self, data) -> None:
+  def enqueue(self, data) -> None:
     # Write your code here
+    if self.last is None:
+      self.head = Node(data)
+      self.last = self.head
+    else:
+      self.last.next = Node(data)
+      self.last = self.last.next
 
-  def pop(self) -> None:
+  def dequeue(self) -> None:
     # Write your code here
-
-  def status(self):
-    """
-    It prints all the elements of stack.
-    """
-    # Write your code here  
+    if self.head is None:
+      return None
+    else:
+      removed_data = self.head.data
+      self.head = self.head.next
+    
+  def status(self) -> None:
+    # Write your code here
+    curr = self.head
+    display = []
+    while (curr):
+      display.append(curr.data)
+      curr = curr.next
+    for ele in display:
+      print(ele, end="=>")
+    print(None)
 
 
 # Do not change the following code
-stack = Stack()
+queue = Queue()
 operations = []
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
 data = input_data.split(',')
 for i in range(len(operations)):
-  if operations[i] == "push":
-    stack.push(int(data[i]))
-  elif operations[i] == "pop":
-    stack.pop()
-stack.status()
+  if operations[i] == "enqueue":
+    queue.enqueue(int(data[i]))
+  elif operations[i] == "dequeue":
+    queue.dequeue()
+queue.status()
